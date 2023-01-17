cc3fe5d44330713b263b5fcdedfbd802f3c4da91
dishanSinghMehta <122879377+dishanSinghMehta@users.noreply.github.com>
2023-01-17 18:18:05 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component(
+  {
+    selector : 'app-root',
+    tempelateUrl : './app.component.html'
+     
+   `<h1>My first App </h1>`
+
+  }
+)
+export class AppComponent
+
+{
+
+}
